421a5fe0eeb70b7a9d24a6b0c58bb73076f47ebb
I326719 <i.kolotov@sap.com>
2018-04-12 11:21:10 +0400

    Initial commit

---
+++ src/main/java/com/example/StackExchange/StackExchangeApplication.java
@@ -0,0 +1,12 @@
+package com.example.StackExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackExchangeApplication.class, args);
+	}
+}
